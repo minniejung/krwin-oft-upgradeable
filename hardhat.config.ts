66f6d859b77469edc9e476616be3f16653f413e5
minniejung <jungmignonne@gmail.com>
2025-07-23 15:43:13 +0900

    feat: add hardhat, layerzero configs

---
+++ hardhat.config.ts
@@ -0,0 +1,106 @@
+// Get the environment configuration from .env file
+//
+// To make use of automatic environment setup:
+// - Duplicate .env.example file and name it .env
+// - Fill in the environment variables
+import 'dotenv/config'
+
+import '@openzeppelin/hardhat-upgrades'
+import 'hardhat-deploy'
+import '@nomiclabs/hardhat-waffle'
+import 'hardhat-deploy-ethers'
+import 'hardhat-contract-sizer'
+import '@nomiclabs/hardhat-ethers'
+import '@layerzerolabs/toolbox-hardhat'
+
+import { HardhatUserConfig, HttpNetworkAccountsUserConfig } from 'hardhat/types'
+
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+
+import './tasks/sendOFT'
+
+// Set your preferred authentication method
+//
+// If you prefer using a mnemonic, set a MNEMONIC environment variable
+// to a valid mnemonic
+const MNEMONIC = process.env.MNEMONIC
+
+// If you prefer to be authenticated using a private key, set a PRIVATE_KEY environment variable
+const PRIVATE_KEY = process.env.PRIVATE_KEY_METAMASK
+
+const accounts: HttpNetworkAccountsUserConfig | undefined = MNEMONIC
+    ? { mnemonic: MNEMONIC }
+    : PRIVATE_KEY
+      ? [PRIVATE_KEY]
+      : undefined
+
+if (accounts == null) {
+    console.warn(
+        'Could not find MNEMONIC or PRIVATE_KEY environment variables. It will not be possible to execute transactions in your example.'
+    )
+}
+
+const config: HardhatUserConfig = {
+    paths: {
+        cache: 'cache/hardhat',
+    },
+    solidity: {
+        version: '0.8.22',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 50,
+            },
+            evmVersion: 'paris',
+        },
+    },
+    networks: {
+        // 'optimism-testnet': {
+        //     eid: EndpointId.OPTSEP_V2_TESTNET,
+        //     url: process.env.RPC_URL_OP_SEPOLIA || 'https://optimism-sepolia.gateway.tenderly.co',
+        //     accounts,
+        // },
+        // 'arbitrum-testnet': {
+        //     eid: EndpointId.ARBSEP_V2_TESTNET,
+        //     url: process.env.RPC_URL_ARB_SEPOLIA || 'https://arbitrum-sepolia.gateway.tenderly.co',
+        //     accounts,
+        // },
+        // 'kaia-testnet': {
+        //     eid: EndpointId.KLAYTN_V2_TESTNET,
+        //     url: process.env.RPC_URL_KAIA_TESTNET || 'https://rpc.testnet.kaia.network',
+        //     accounts,
+        // },
+        // 'fuji-testnet': {
+        //     eid: EndpointId.AVALANCHE_V2_TESTNET,
+        //     url: process.env.RPC_URL_FUJI_TESTNET || 'https://api.avax-test.network/ext/bc/C/rpc',
+        //     accounts,
+        // },
+        'sepolia-testnet': {
+            eid: EndpointId.SEPOLIA_V2_TESTNET,
+            url: process.env.RPC_URL_SEPOLIA_TESTNET || 'https://rpc.sepolia.org',
+            accounts,
+        },
+        'base-testnet': {
+            eid: EndpointId.BASESEP_V2_TESTNET,
+            url: process.env.RPC_URL_BASE_TESTNET || 'https://sepolia.base.org',
+            accounts,
+        },
+        hardhat: {
+            // Need this for testing because TestHelperOz5.sol is exceeding the compiled contract size limit
+            allowUnlimitedContractSize: true,
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // wallet address of index[0], of the mnemonic in .env
+        },
+    },
+    layerZero: {
+        // You can tell hardhat toolbox not to include any deployments (hover over the property name to see full docs)
+        deploymentSourcePackages: [],
+        // You can tell hardhat not to include any artifacts either
+        // artifactSourcePackages: [],
+    },
+}
+
+export default config
