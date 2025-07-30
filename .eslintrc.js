a537e7a74236ad49d46233b3afc0ca33c1441b22
minniejung <jungmignonne@gmail.com>
2025-07-30 10:15:10 +0900

    feat: add eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,16 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 module.exports = {
     root: true,
     extends: ['@layerzerolabs/eslint-config-next/recommended'],
+    plugins: [
+        'import'
+      ],
     rules: {
         // @layerzerolabs/eslint-config-next defines rules for turborepo-based projects
         // that are not relevant for this particular project
         'turbo/no-undeclared-env-vars': 'off',
-        'import/no-unresolved': 'warn', // lint runs before workspace packages are built; missing dist/ folders cause false unresolved errors
+        'import/no-unresolved': 'warn',
+        'import/order': 'error',
+        'import/no-duplicates': 'error', 
+        'import/no-unused-modules': 'warn', 
     },
 };
